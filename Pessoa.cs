1ee0f237f60a6a4556861174509b0cd109fb5c3e
Lramos91 <lucas.ramosdavid@gmail.com>
2022-08-02 21:25:04 -0300

    first commit

---
+++ Pessoa.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sistemaUc12
+{
+    public class Pessoa
+    {
+        //Atributos
+        public string? nome { get; set; }
+        public string? Endereco { get; set; }
+        public bool enderecoComercial { get; set; }
+
+        //Metodos
+        public void PagarImposto(){}
+
+        
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace sistemaUc12
+{
+    
+class program{
+
+static void Main(string[] args)
+     {
+        Pessoa lucas = new Pessoa ();
+        Pessoa caique = new Pessoa ();
+        Pessoa andre = new Pessoa ();
+        Pessoa daniel = new Pessoa ();
+
+        lucas.Endereco = "rua x";
+        lucas.nome = "lucas ramos";
+
+        Console.WriteLine(lucas.Endereco);
+        Console.WriteLine(lucas.nome);
+
+     }
+    }
+}
